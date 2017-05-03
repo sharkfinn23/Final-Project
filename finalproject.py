cf9453dd0e3c2a0d3b8f76bbcb8565c410a140f4
sharkfinn23 <thomashackett@hanovernorwichschools.org>
2017-05-03 13:52:57 -0400

    Updated from Brython Server: 5/3/2017 1:52:57 PM

--- finalproject.py
+++ finalproject.py
@@ -1,1 +1,42 @@
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+blue = Color(0x2EFEC8, 1.0)
+black = Color(0x000000, 1.0)
+pink = Color(0xFF00FF, 1.0)
+red = Color(0xFF5733, 1.0)
+white = Color(0xFFFFFF, 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+white = Color(0xffffff, 1.0)
+grey = Color(0xC0C0C0, 1.0)
+
+thinline = LineStyle(2, black)
+blkline = LineStyle(1, black)
+noline = LineStyle(0, white)
+coolline = LineStyle(1, grey)
+blueline = LineStyle(2, blue)
+redline = LineStyle(1, red)
+greenline = LineStyle(1, green)
+gridline = LineStyle(1, grey)
+grid=RectangleAsset(30,30,gridline,white)
+
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, green)
+bg = Sprite(bg_asset, (0,0))
+
+class Road(Sprite):
+    Road = RectangleAsset(200, 20, thinline, black)
+        
+
+class Frogger(App):
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+        super().__init__()
+        self.mousex = 0
+        self.mousey = 0
+        self.listenKeyEvent('keydown', 's', self.go)
+
+def go(self, event):
+    Road(0,0)
